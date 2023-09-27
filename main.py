1242e6287839963dbcb59085e8d2ef206ba56134
matt coleman <matthew.coleman@southerninnovation.com>
2023-09-27 16:15:26 +1000

    Updated to new selenium API. Now saves all dates across multiple pages. Only saves images that don't already exist in IMAGE PATH'

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import logging
+import itertools
 
 import os, json, sys, argparse, time, pathlib
 
@@ -12,6 +14,25 @@ try:
     from get_gecko_driver import GetGeckoDriver
 except BaseException:
     getGecko_installed = False
+
+
+def config_logger(logger: logging.Logger) -> logging.Logger:
+    """
+    Standardise logging output
+    """
+
+    logger.setLevel(logging.INFO)
+    logger.propogate = False
+
+    formatter = logging.Formatter('%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s]: %(message)s')
+
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.setFormatter(formatter)
+
+    logger.addHandler(stdout_handler)
+    return logger
+
+_logger = config_logger(logging.getLogger(__name__))
 
 class Scraper:
 
@@ -53,39 +74,33 @@ class Scraper:
         driver.get(self.MB_URL)
 
         # login
-        driver.find_element_by_id("loginDropdown").click()
-        driver.find_element_by_id("Login_Username").send_keys(USERNAME)
-        driver.find_element_by_id("Login_Password").send_keys(PASSWORD)
-        driver.find_element_by_id("navlogin").click()
+        driver.find_element(By.ID, "loginDropdown").click()
+        driver.find_element(By.ID, "Login_Username").send_keys(USERNAME)
+        driver.find_element(By.ID, "Login_Password").send_keys(PASSWORD)
+        driver.find_element(By.ID, "navlogin").click()
 
-        WebDriverWait(
-            driver, 10).until(
-            EC.presence_of_element_located(
-                (By.ID, "navlogin")))
+        # WebDriverWait(
+        #     driver, 10).until(
+        #     EC.presence_of_element_located(
+        #         (By.ID, "navlogin")))
         #WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "element_id")))
-        time.sleep(10)
+        time.sleep(5)
         # navigate to logbook
         # driver.get("https://moonboard.com/Logbook/Index")
-        driver.find_element_by_id("llogbook").click()
-        driver.find_element_by_link_text("VIEW").click()
+        driver.find_element(By.ID,"llogbook").click()
+        time.sleep(0.5)
+        driver.find_element(By.LINK_TEXT, "VIEW").click()
 
-        time.sleep(10)  # TODO: await properly
+        time.sleep(5)  # TODO: await properly
 
         # select version
-        select = Select(driver.find_element_by_id("Holdsetup"))
+        select = Select(driver.find_element(By.ID,"Holdsetup"))
         select.select_by_index(3)  # 2019 version TODO : more stable
 
         time.sleep(5)  # TODO: await properly
 
-        main_section = driver.find_element_by_id("main-section")
 
-        # get headers for rows
-        headers = main_section.find_elements_by_class_name(
-            "logbook-grid-header")
-
-        # get a tag expanders for the various days
-        expanders = driver.find_elements_by_xpath(
-            "//a[@class='k-icon k-i-expand']")
+        
         # Lag
         time.sleep(3)
 
@@ -93,32 +108,78 @@ class Scraper:
         res = []
         def any_nested(list, key): return any([k == key for k, _, _ in list])
         
-        for a_tag, header in zip(expanders, headers):
-            a_tag.click()  # expand information for that day
-            # Lag
-            time.sleep(1)
-
-        entries = main_section.find_elements_by_class_name("entry")
-        entry_text = [e.text.split('\n')[0] for e in entries]
-
-        unique_entries = [(entries[i],entry_text[i]) for i in range(1, len(entries)) 
-                            if i==1 or (entry_text[i] != entry_text[i-1])]
+        # 
         
+        # Each element is a page that contains up to 40 dates 
+        # does not include the first page, currently, selected, element XPATh 'k-state-selected'
+        page_elements = driver.find_elements(By.XPATH,"//a[@class='k-link']")
 
-        print(f"#Entries: {len(unique_entries)} Problems:{' '.join([e[1] for e in unique_entries])}")
-        for entry,entry_name in unique_entries:
-            # Get screenshot
-            entry.click()
-            time.sleep(1)
-            img_path = os.path.join(self.IMAGE_PATH,entry_name.replace(" ","_")+".png")
-            if not os.path.isfile(img_path):
-                driver.get_screenshot_as_file(img_path)       
+        _logger.info(f"{len(page_elements) + 1} pages of dates found in logbook..")
+
+        # Iterate through pages, and then dates
+        for i, _ in enumerate(page_elements):
+            # Can't directly work with page elements as they go stale
+
+            _logger.info(f"Processing page {i+1}")
+            # Used to find individual entries
+            main_section = driver.find_element(By.ID,"main-section")
+
+            # get headers for each clickable date - can be used to find the date text. 
+            headers = main_section.find_elements(
+                By.CLASS_NAME, "logbook-grid-header")
+
+            # Generate a list of dates repeated based on number of problems for that day (for iterating over later)
+            # This will be used later
+            date_texts = [h.text.split('\n')[0] for h in headers]
+            num_problems_per_date = [int(h.text.split('\n')[1][:2]) for h in headers]
+            repeated_date_list = itertools.chain.from_iterable(
+                itertools.repeat(value, count) 
+                for value, count in zip(date_texts, num_problems_per_date)
+            )
             
-            # get data
-            if not any_nested(res, entry.text):
-                res_entry = (entry.text, header.text, img_path)  # (route info, date)
-                res.append(res_entry)
 
+            # get a tag expanders for the various days
+            date_expanders = main_section.find_elements(
+                By.XPATH, "//a[@class='k-icon k-i-expand']")
+
+            # Click to expand all dates
+            for date_expander, header in zip(date_expanders, headers):
+                date_expander.click()
+                time.sleep(1)
+
+            # SHould contain all entries for the entire page
+            entries = main_section.find_elements(By.CLASS_NAME, "entry")
+            entry_names = [e.text.split('\n')[0] for e in entries]
+
+            # Logbook error has dupes (doubles) of entries in consecutive slots
+
+            # TODO: Better way to do this.. But the ids arent duped.. just text
+            # unique_entries = [(entries[i],entry_text[i]) for i in range(1, len(entries)) 
+            #                     if i==1 or (entry_text[i] != entry_text[i-1])]
+            entries = entries[::2]
+
+            print(f"Page {i + 1} - #Entries: {len(entries)} ")
+            for (entry, entry_date) in zip(entries, repeated_date_list):
+                entry_name = entry.text.split('\n')[0]
+                img_path = os.path.join(self.IMAGE_PATH, entry_name.replace(" ","_")+".png")
+                if not os.path.isfile(img_path):
+                    # Get screenshot only if the file doesn't exist
+                    # TODO: Update when using rdbms
+                    entry.click()
+                    time.sleep(1)
+                    driver.get_screenshot_as_file(img_path)       
+                
+                # get data
+                if not any_nested(res, entry.text):
+                    res_entry = (entry.text, entry_date, img_path)  # (route info, date)
+                    res.append(res_entry)
+            
+            # Navigate to the next page by clicking on the number
+            next_page_clicker = driver.find_elements(By.XPATH,"//a[@class='k-link k-pager-nav']")[0]
+            next_page_clicker.click()
+            time.sleep(5)
+                
+        _logger.info(f"Completed scraping {len(res)} entries. Formatting and storing..")
         # process data
         formatted_data = []
         for (data, date, img_name) in res:
